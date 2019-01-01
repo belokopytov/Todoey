4ac79ad1aabf6fa378418f6e52d7ed0f6dc1f3aa
Mikhail Belokopytov <belokopytov@gmail.com>
2019-01-02 01:00:12 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mikhail Belokopytov on 02/01/2019.
+//  Copyright © 2019 mbel. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false 
+    
+}
